5bae13be2dbd9fcecbd4f4eb48ebb11d05029b1c
3aaap <sdz96966@126.com>
2015-09-20 17:19:59 +0800

    完成功能

--- 06-仿SDWebImage（Git）/HMAppModel.h
+++ 06-仿SDWebImage（Git）/HMAppModel.h
@@ -6,7 +6,7 @@
 //  Copyright © 2015年 宋得中. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 
 @interface HMAppModel : NSObject
 
---
+++ 06-仿SDWebImage（Git）/NSString+CachePath.h
@@ -0,0 +1,15 @@
+//
+//  NSString+CachePath.h
+//  06-仿SDWebImage（Git）
+//
+//  Created by 宋得中 on 15/9/20.
+//  Copyright © 2015年 宋得中. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (CachePath)
+
+- (NSString*)getImagePath;
+
+@end
